b0162ab914589f999a8dc52dec9fda4e731a9172
tomike26 <kolajotomike26@gmailcom>
2024-06-08 13:59:00 +0100

    Linking Pages

---
+++ app/about/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+
+const AboutPage = () => {
+  return (
+    <div className="text-center">
+        <h1>AboutPage</h1>
+        <Link href='/'>HOME PAGE</Link>
+        </div>
+  )
+}
+
+export default AboutPage
